cadf889c91d7a28ec753b507d4f036e04b45f23d
YtalloXD <r.ytalofelix11@gmail.com>
2026-01-10 16:38:12 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x.layout>
+    <x.slot:title>
+        Welcome
+    </x.slot:title>
+    <div class="max-w-2xl mx-auto">
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <h1 class="text-3xl font-bold">Welcome to IM!</h1>
+                        <p class="mt-4 text-base-content/60">It's a (ficticious) social media app featuring your favorite characters.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</x.layout>
